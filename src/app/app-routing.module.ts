362e9622c0833f5dad37469cf209cee51b90025d
Arthur Quemeras <arthur.quemeras@gmail.com>
2024-03-11 11:41:31 +0100

    fin footer et header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AccueilComponent } from "./accueil/accueil.component";
+import { CvComponent } from "./cv/cv.component";
+import { TestComponent } from "./test/test.component";
 
-const routes: Routes = [{ path: "", component: AccueilComponent }];
+const routes: Routes = [
+  { path: "cv", component: CvComponent },
+  { path: "test", component: TestComponent },
+  { path: "", component: AccueilComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
+  navLinks!: any[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.navLinks = [
+      {
+        link: "",
+        label: "Accueil",
+        description: "Accueil",
+      },
+      {
+        link: "cv",
+        label: "Curriculum vitae",
+        description: "curriculum vitae",
+      },
+      {
+        link: "test",
+        label: "Test",
+        description: "Test",
+      },
+    ];
   }
 
+  ngOnInit(): void {}
 }
