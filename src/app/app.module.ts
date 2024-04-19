aef97f64b06f914d3e8dfcc494173a690eeedd18
Arthur Quemeras <arthur.quemeras@gmail.com>
2024-04-19 10:59:52 +0200

    divers modifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
-import { ReactiveFormsModule } from "@angular/forms";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatIconModule } from "@angular/material/icon";
-import { MatProgressBarModule } from "@angular/material/progress-bar";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { AccueilComponent } from "./accueil/accueil.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ContactComponent } from "./contact/contact.component";
-import { CvComponent } from "./cv/cv.component";
-import { FooterComponent } from "./footer/footer.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { AccueilComponent } from "./accueil/accueil.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { CvComponent } from "./cv/cv.component";
+import { TestComponent } from "./test/test.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
 import { ItemComponent } from "./item/item.component";
 import { PopupComponent } from "./popup/popup.component";
-import { PortfolioComponent } from "./portfolio/portfolio.component";
-import { TestComponent } from "./test/test.component";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatIconModule } from "@angular/material/icon";
+import { ContactComponent } from "./contact/contact.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -42,7 +43,7 @@ import { TestComponent } from "./test/test.component";
     ReactiveFormsModule,
     MatProgressBarModule,
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
