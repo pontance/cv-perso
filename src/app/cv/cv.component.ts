efcefaa09e9e6dd404bcca50b73164d8e55c719f
Arthur Quemeras <arthur.quemeras@gmail.com>
2024-04-24 13:37:15 +0200

    qr code + animation

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from "@angular/core";
 
 @Component({
   selector: "app-cv",
@@ -6,7 +6,20 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./cv.component.scss"],
 })
 export class CvComponent implements OnInit {
+ 
+  @ViewChild('icons') private icons!: ElementRef<HTMLDivElement>;
+  isVisible: boolean =false;
   constructor() {}
 
   ngOnInit(): void {}
+
+  @HostListener('window:scroll', ['$event'])
+  isScrolledIntoView(){
+    if (this.icons){
+      const rect = this.icons.nativeElement.getBoundingClientRect();
+      const topShown = rect.top >= 0;
+      const bottomShown = rect.bottom <= window.innerHeight;
+      this.isVisible = topShown && bottomShown;
+    }
+  }
 }
